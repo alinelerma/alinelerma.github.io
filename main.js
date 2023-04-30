2fde9e9cfc000e2495e381441905fe3cd2f4c3bd
Aline Lerma <130856553+alinelerma@users.noreply.github.com>
2023-04-30 17:02:34 -0600

    Add files via upload
    Initial commit HTML+CSS+JS

---
+++ main.js
@@ -0,0 +1,35 @@
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+ 
+typewriter
+  .pauseFor(2000)
+  .typeString('Desarrollo de sitios web, Divulgación y Física')
+  .pauseFor(500)
+  .deleteChars(10)
+  .start();
+
+
+  let slideIndex = [1,1];
+let slideId = ["mySlides1", "mySlides2"]
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
